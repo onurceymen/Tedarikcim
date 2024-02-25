b1b4cc0b606e2f211b14d446bcce7c90912c8598
onurceymen <onurceymen@yahoo.com>
2024-02-25 22:43:07 +0300

    Add Identity User Login Register Operations

---
+++ TedarikcimUser/Areas/Identity/Data/TedarikcimUserUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace TedarikcimUser.Areas.Identity.Data;
+
+// Add profile data for application users by adding properties to the TedarikcimUserUser class
+public class TedarikcimUserUser : IdentityUser
+{
+    [PersonalData]
+    [Column(TypeName ="nvarchar(100)")]
+    public string FirstName { get; set; }
+
+    [PersonalData]
+    [Column(TypeName = "nvarchar(100)")]
+    public string LastName { get; set; }
+}
+
